4d9166527e85bd5257834330682e26a121ccbd4c
MadhusudanY <madish166@gmail.com>
2023-05-17 13:35:01 +0530

    3/29 : First Commit

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ import re
 
 import processWord
 
+
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
 if __name__ == '__main__':
     processWord.execute()
-    # sample_str = "	This work is performed indoors Page 113 of 1031 and out-of-doors, and includes night and
-    # weekend  " \ "duties Page 2 of 5" sample_str = re.sub(r'Page [0-9]+ of [0-9]+', '', sample_str) print(f'"{
-    # sample_str}"')
--- processWord.py
+++ processWord.py
@@ -1,7 +1,11 @@
-import docx2txt
+import os
 import re
 from pathlib import Path
+
+import docx2txt
 from openpyxl import Workbook
+
+from PyPDF2 import PdfReader
 
 
 # Function to get folder containing position descriptions documents from the console input
@@ -11,29 +15,12 @@ def get_path():
     return s.replace('\\', '/'), c
 
 
-def massage_word_data(client_name, str_content):
-    if client_name == "FEATHER RIVER COMMUNITY COLLEGE" or client_name == "FRC":
-        str_content = re.sub(r'Page [0-9]+ of [0-9]+', '', str_content)
-        # print(str_content)
-        str_content = str_content.replace("FEATHER RIVER", "").replace("COMMUNITY COLLEGE DISTRICT", "") \
-            .replace("570 Golden Eagle Ave., Quincy CA 95971", "").replace("(530) 283-0202, ext. 257", "").replace(
-            "www.frc.edu", "").strip()
-        return str_content
-    elif client_name == "COCCD":
-        str_content = re.sub(r'Page [0-9]+ of [0-9]+', '', str_content)
-        # print(str_content)
-        str_content = str_content.replace("FEATHER RIVER", "").replace("COMMUNITY COLLEGE DISTRICT", "") \
-            .replace("570 Golden Eagle Ave., Quincy CA 95971", "").replace("(530) 283-0202, ext. 257", "").replace(
-            "www.frc.edu", "").strip()
-        return str_content
-
-
 def get_headers(client_name):
+    headers_dict = {}
     with open('Headers.txt', 'r') as f:
         for line in f.readlines():
             if line.startswith(client_name):
                 headers_list = line.split('|')
-                headers_dict = {}
                 for x in headers_list[1:]:
                     if x.count('-') == 0:
                         headers_dict[x] = ""
@@ -41,6 +28,7 @@ def get_headers(client_name):
                         x = x.split("-")[0]
                         headers_dict[x] = ""
                 f.close()
+                break
     return headers_dict
 
 
@@ -71,6 +59,10 @@ def get_col_data(client, headers, data):
                 start_index = data.lower().index(marker1.lower()) + len(marker1)
                 end_index = data.lower().index(marker2.lower())
                 col_data = data[start_index:end_index].strip()
+            elif data.count(marker1) > 0 and data.count(marker2) > 0:
+                start_index = data.index(marker1) + len(marker1)
+                end_index = data.index(marker2)
+                col_data = data[start_index:end_index].strip()
         headers[marker1] = col_data
     if data.lower().count(marker2.lower()) > 0:
         start_index = data.lower().index(marker2.lower()) + len(marker2)
@@ -83,30 +75,43 @@ def get_col_data(client, headers, data):
     return headers
 
 
-def modify_operation(value, modifiers, dict_headers):
-    for item in modifiers:
-        if item.startswith('replaceWithEmpty'):
-            a, b = item.split(' ')
-            value = value.replace(b, '')
-        if item.startswith('sliceLastIndexOf'):
-            a, b, c = item.split(' ')
-            c = int(c)
-            if c == 0:
-                c = -1
-            value = value[len(value) - value[::-1].index(b):]
-        if item.startswith('replaceAsContains'):
-            a, b, c = item.split(' ')
-            if value.lower().__contains__(b.lower()):
-                value = b
-            elif value.lower().__contains__(c.lower()):
-                value = c
-        if item.startswith('replaceFromKey'):
-            key = item[item.index('(')+1:item.index(')')]
-            value = dict_headers.get(key)
-            a, b = item.replace(f'replaceFromKey({key}) ', '').split('-')
-            if a == 'AlphaNumericOneChar':
-                value = re.findall("[a-zA-Z0-9]", value)[int(b)]
-    return value.strip()
+def modify_operation(col_header, modify_item, dict_headers):
+    value = dict_headers.get(col_header)
+    if modify_item.startswith('replaceWithEmpty'):
+        a, b = modify_item.split(' ')
+        value = value.replace(b, '')
+        dict_headers[col_header] = value
+    if modify_item.startswith('sliceLastIndexOf'):
+        a, b, c = modify_item.split(' ')
+        c = int(c)
+        if c == 0:
+            c = -1
+        value = value[len(value) - value[::-1].index(b):]
+        dict_headers[col_header] = value
+    if modify_item.startswith('replaceAsContains'):
+        a, b, c = modify_item.split(' ')
+        if value.lower().__contains__(b.lower()):
+            value = b
+        elif value.lower().__contains__(c.lower()):
+            value = c
+        dict_headers[col_header] = value
+    if modify_item.startswith('replaceFromKey'):
+        key = modify_item[modify_item.index('(') + 1:modify_item.index(')')]
+        value = dict_headers.get(key)
+        a, b = modify_item.replace(f'replaceFromKey({key}) ', '').split('-')
+        if a == 'AlphaNumericOneChar':
+            value = re.findall("[a-zA-Z0-9]", value)[int(b)]
+        dict_headers[col_header] = value
+    if modify_item.startswith('BreakBacK'):
+        key_list = list(dict_headers)
+        key_index = key_list.index(col_header)
+        values_reverse = value.split('\n\n')[::-1]
+        for x in values_reverse:
+            dict_headers[key_list[key_index]] = x
+            key_index = key_index - 1
+    if modify_item.startswith('LTrim'):
+        dict_headers[col_header] = value.lstrip()
+    return dict_headers
 
 
 def apply_modifiers(client, dict_headers):
@@ -118,47 +123,60 @@ def apply_modifiers(client, dict_headers):
                 f.close()
                 break
     for item in modifiers_list:
-        col_header = item.split('#')[0]
-        modifiers = item.split('#')[1:]
-        dict_headers[col_header] = modify_operation(dict_headers.get(col_header), modifiers, dict_headers)
+        key, mod = item.split('#')
+        dict_headers = modify_operation(key, mod, dict_headers)
     return dict_headers
 
 
-def execute(tb=None):
+def begin_process(client, text_data, filename, ws, row):
+    # This is for debugging Purpose
+    # print(text_data)
+    # exit(0)
+    dict_headers = get_headers(client)
+    dict_headers['FILENAME'] = filename
+    # Last line Item on Header.txt is the string value that needs to be excluded. I called it chug_it value.
+    # Hence, pop it from dict_headers
+    dict_headers.popitem()
+    dict_headers = get_col_data(client, dict_headers, text_data)
+    # This is for debugging Purpose
+    # print(dict_headers)
+    dict_headers = apply_modifiers(client, dict_headers)
+    col = 1
+    for k in dict_headers.keys():
+        ws.cell(1, col).value = k.replace(":", "")
+        ws.cell(row, col).value = dict_headers.get(k)
+        col += 1
+    row += 1
+    return ws, row
+
+
+def execute():
     pd_dir, client = get_path()
-    directory = Path(pd_dir).glob("*.docx")
     wb = Workbook()
     xl_file = f"{pd_dir}/PositionDescriptions.xlsx"
     ws = wb.create_sheet(index=0, title="PositionDescriptions")
     row = 2
     try:
-        for my_file in directory:
-            word_data = docx2txt.process(my_file)
-            # This is for debugging Purpose
-            print(word_data)
-            # exit(0)
-            dict_headers = get_headers(client)
-            dict_headers['FILENAME'] = my_file.name
-            # Last line Item on Header.txt is the string value that needs to be excluded. I called it chug_it value.
-            # Hence pop it from dict_headers
-            dict_headers.popitem()
-            dict_headers = get_col_data(client, dict_headers, word_data)
-            # This is for debugging Purpose
-            print(dict_headers)
-            dict_headers = apply_modifiers(client, dict_headers)
-            try:
-                col = 1
-                for k in dict_headers.keys():
-                    ws.cell(1, col).value = k.replace(":", "")
-                    ws.cell(row, col).value = dict_headers.get(k)
-                    col += 1
-                row += 1
-            except IndexError:
-                print("Something went wrong with " + my_file.name)
-                continue
-        print("Execution Completed")
+        for file in os.listdir(pd_dir):
+            if file.endswith('.docx'):
+                word_data = docx2txt.process(pd_dir+'\\' + file)
+                try:
+                    ws, row = begin_process(client, word_data, file, ws, row)
+                except IndexError:
+                    print("Something went wrong with " + file)
+                    continue
+            if file.endswith('.pdf'):
+                with open(pd_dir+'\\' + file, mode='rb') as x:
+                    reader = PdfReader(x)
+                    pdf_data = ""
+                    for i in range(len(reader.pages)):
+                        page = reader.pages[i]
+                        pdf_data = pdf_data + page.extract_text()
+                    x.close()
+                # print(pdf_data)
+                ws, row = begin_process(client, pdf_data, file, ws, row)
     except PermissionError as p:
-        print("Permission denied. Please see the docx file(s) are closed.")
-        raise Exception(print("Something went wrong :( "))
+        print("Permission denied. Please see the docx file(s) are closed." + p.strerror)
     finally:
         wb.save(xl_file)
+        print("Execution Complete")
